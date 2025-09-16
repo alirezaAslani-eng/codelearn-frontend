1fc2291750656e0d6843b1ffd3cc69cf080477d3
Alireza <alirezaaslane.mob7@gmail.com>
2025-09-16 17:00:56 +0330

    deploy

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -22,105 +22,108 @@ const BlogInfoContainer = lazy(() =>
 const SessionInfoContainer = lazy(() =>
   import("../container/pagesContainer/SessionInfoContainer")
 );
-const routes = createBrowserRouter([
-  { path: "/", element: <Navigate to={"/" + Main.root} /> },
-  {
-    path: Main.root,
-    element: <RootLayout />,
-    children: [
-      // Main-Page ----- >
-      {
-        index: true,
-        element: <Home />,
-      },
-      // SingnUp-Page -- >
-      {
-        path: Main.register,
-        element: <Register />,
-      },
-      // Login-Page -- >
-      {
-        path: Main.login,
-        element: <Login />,
-      },
-      // ContactUs-Page -- >
-      {
-        path: Main.contact,
-        element: (
+const routes = createBrowserRouter(
+  [
+    { path: "/", element: <Navigate to={"/" + Main.root} /> },
+    {
+      path: Main.root,
+      element: <RootLayout />,
+      children: [
+        // Main-Page ----- >
+        {
+          index: true,
+          element: <Home />,
+        },
+        // SingnUp-Page -- >
+        {
+          path: Main.register,
+          element: <Register />,
+        },
+        // Login-Page -- >
+        {
+          path: Main.login,
+          element: <Login />,
+        },
+        // ContactUs-Page -- >
+        {
+          path: Main.contact,
+          element: (
+            <Suspense>
+              <ContactUs />
+            </Suspense>
+          ),
+        },
+        // Category-Page -- >
+        {
+          path: Main.category + "/:categoryName",
+          element: <CategoryContainer />,
+        },
+        // Courses-Page -- >
+        {
+          path: Main.courses,
+          element: <AllCoursesContainer />,
+        },
+        // Info of a Course-Page -- >
+        {
+          path: Main.course_info + "/:courseName",
+          element: (
+            <Suspense>
+              <CourseInfoContainer />
+            </Suspense>
+          ),
+        },
+        // Info of a session-Page -- >
+        {
+          path: Main.session_info + "/:courseShortName/:sessionId",
+          element: (
+            <Suspense>
+              <SessionInfoContainer />
+            </Suspense>
+          ),
+        },
+        // Blogs-Page -- >
+        {
+          path: Main.blogs,
+          element: <AllBlogsContainer />,
+        },
+        // Info of a blog-Page -- >
+        {
+          path: Main.blog_info + "/:blogName",
+          element: (
+            <Suspense>
+              <BlogInfoContainer />
+            </Suspense>
+          ),
+        },
+        // final buy-Page -- >
+        {
+          path: Main.buy_course,
+          element: <BuyCoursePage />,
+        },
+        // globasl result of a serach-Page -- >
+        {
+          path: Main.globalSearch + "/:searchValue",
+          element: <GlobalSearchContainer />,
+        },
+        // chat-Page -- >
+        {
+          path: Main.chat,
+          element: <MobileChat />,
+        },
+      ],
+    },
+    // User Panel-Root --->
+    {
+      path: userPanel.root + "/*",
+      element: (
+        <UserPanelPrivet>
           <Suspense>
-            <ContactUs />
+            <RootUserPanel />
           </Suspense>
-        ),
-      },
-      // Category-Page -- >
-      {
-        path: Main.category + "/:categoryName",
-        element: <CategoryContainer />,
-      },
-      // Courses-Page -- >
-      {
-        path: Main.courses,
-        element: <AllCoursesContainer />,
-      },
-      // Info of a Course-Page -- >
-      {
-        path: Main.course_info + "/:courseName",
-        element: (
-          <Suspense>
-            <CourseInfoContainer />
-          </Suspense>
-        ),
-      },
-      // Info of a session-Page -- >
-      {
-        path: Main.session_info + "/:courseShortName/:sessionId",
-        element: (
-          <Suspense>
-            <SessionInfoContainer />
-          </Suspense>
-        ),
-      },
-      // Blogs-Page -- >
-      {
-        path: Main.blogs,
-        element: <AllBlogsContainer />,
-      },
-      // Info of a blog-Page -- >
-      {
-        path: Main.blog_info + "/:blogName",
-        element: (
-          <Suspense>
-            <BlogInfoContainer />
-          </Suspense>
-        ),
-      },
-      // final buy-Page -- >
-      {
-        path: Main.buy_course,
-        element: <BuyCoursePage />,
-      },
-      // globasl result of a serach-Page -- >
-      {
-        path: Main.globalSearch + "/:searchValue",
-        element: <GlobalSearchContainer />,
-      },
-      // chat-Page -- >
-      {
-        path: Main.chat,
-        element: <MobileChat />,
-      },
-    ],
-  },
-  // User Panel-Root --->
-  {
-    path: userPanel.root + "/*",
-    element: (
-      <UserPanelPrivet>
-        <Suspense>
-          <RootUserPanel />
-        </Suspense>
-      </UserPanelPrivet>
-    ),
-  },
-]);
+        </UserPanelPrivet>
+      ),
+    },
+  ],
+  { basename: "/codelearn-frontend" }
+);
 export { routes };
