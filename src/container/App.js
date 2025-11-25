e3909e717a218f7f274356bd88913574808da3aa
Alireza <alirezaaslane.mob7@gmail.com>
2025-11-25 19:06:50 +0330

    feat() show an alert while mounting app to tell user to turn on VPN

--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "../routes/routes";
 import { initialSetTheme } from "../utils";
+import { useEffect } from "react";
 
 export default function App() {
   // dark light theme -- >
   initialSetTheme();
+  useEffect(() => {
+    alert("Please turn on your vpn !!");
+  }, []);
   return (
     <div id="App" className="min-h-screen">
       <RouterProvider router={routes} />
