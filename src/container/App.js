1c3197b766964a1dc89694bce05e1e653664df2e
Alireza <alirezaaslane.mob7@gmail.com>
2025-12-21 16:54:39 +0330

    fix(remove alert)

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "../routes/routes";
 import { initialSetTheme } from "../utils";
-import { useEffect } from "react";
-
 export default function App() {
   // dark light theme -- >
   initialSetTheme();
-  useEffect(() => {
-    alert("Please turn on your vpn !!");
-  }, []);
   return (
     <div id="App" className="min-h-screen">
       <RouterProvider router={routes} />
